3a2962a3fa50e597071f6d5c60dedee8ed7575ca
Christopher <chrisberry4545@googlemail.com>
2018-01-02 17:16:45 +0000

    Updated component templates

--- files-to-generate/component/chrisbnametosubhere.module.ts
+++ files-to-generate/component/chrisbnametosubhere.module.ts
@@ -1,11 +1,14 @@
+import { NgModule } from '@angular/core';
 import 'reflect-metadata';
 import 'zone.js';
-import { NgModule } from '@angular/core';
 
 import chrisbnametosubhereCamelCaseComponent from './chrisbnametosubhere.component';
 
 @NgModule({
     declarations: [
+        chrisbnametosubhereCamelCaseComponent,
+    ],
+    exports: [
         chrisbnametosubhereCamelCaseComponent,
     ],
 })
