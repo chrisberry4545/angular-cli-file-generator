d414e1d245101fdf0835762bee47bfd3313e2a3b
Christopher <chrisberry4545@googlemail.com>
2018-05-09 13:39:36 +0100

    feat: added change detection to angular

--- files-to-generate/component/chrisbnametosubhere.component.ts
+++ files-to-generate/component/chrisbnametosubhere.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+} from '@angular/core';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'chrisbnametosubhere-component',
     styleUrls: [
         './chrisbnametosubhere.component.scss',
